cfc83ce43ef89bfa61c240d7e091fdf8137494c4
shubyntsev-dev <shubyntsev1970@gmail.com>
2026-03-29 20:24:26 +0300

    Assert.Equal(6, result) - ввел специально ошибку при проверке

--- CITestExampleAppTest/CalculatorTest.cs
+++ CITestExampleAppTest/CalculatorTest.cs
@@ -23,8 +23,7 @@ namespace CITestExampleAppTest
 
             Console.WriteLine("This test verifies that the Add method correctly sums two integers.");
             // Assert
-            Assert.Equal(5, result);
-
+            Assert.Equal(6, result);
         }
     }
 }
