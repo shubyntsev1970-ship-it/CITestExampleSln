d959fbc37e247ddcb9037eb2455016bfa969ed0d
shubyntsev-dev <shubyntsev1970@gmail.com>
2026-04-04 18:35:36 +0300

    Add  Subtract and test for it

--- CITestExampleApp/Calculator.cs
+++ CITestExampleApp/Calculator.cs
@@ -6,6 +6,12 @@
         {
             return a + b;
         }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
         public int Divide(int a, int b)
         {
             return a / b;
